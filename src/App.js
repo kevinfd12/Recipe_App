14c0ad4705b011a2be15da047cc129a37b868452
Kevinfd12 <kevinfd12@gmail.com>
2022-07-23 10:00:47 -0400

    Initial changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Home from "./pages/Home";
+import Pages from "./pages/Pages";
+
+const App = () => {
+  return (
+    <div className="App">
+        <h1>Hello</h1>
+        <Pages />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+       'process.env': JSON.stringify(process.env)
+    })
+  ]
+};
